eaa2191640065d517efa43b1954282b919f53427
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2020-03-12 15:09:45 -0400

    jpfp refactor: added note for file structure

--- tests/tier-01/02-students.js
+++ tests/tier-01/02-students.js
@@ -27,6 +27,7 @@ const { Student } = require('../../server/db')
 const adapter = new Adapter()
 enzyme.configure({ adapter })
 
+/* NOTE: Make sure you pay attention to the path below. This is where your React components should live! */
 import { AllStudents } from '../../app/components/AllStudents'
 
 describe('Tier One: Students', () => {
